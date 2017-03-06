a982c7f400ceca2d68890684f9d85074b1d60bda
Roger Miller <rogeralexmiller@gmail.com>
2017-03-05 19:43:30 -0500

    move hook to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-
-  def require_api_token
-    unless params[:api_token] == ENV["API_SECRET"]
-      render json: {status: 404, message: "invalid api token"}
-    end
-  end
 end
