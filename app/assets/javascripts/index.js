8a1eb0401114dc2a77a3fa0e43d71f36e98eb512
Roger Miller <rogeralexmiller@gmail.com>
2017-05-08 19:59:57 -0400

    use line drawing to fix animation glitch

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  var testing = true;
+  var testing = false;
 
   var canvas = document.getElementById('temp-canvas');
 
@@ -12,42 +12,41 @@ $(document).ready(function () {
 
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, 300, 200);
 
-    ctx.fillStyle = 'white';
-    ctx.strokeStyle = 'black';
-    ctx.fillRect(0,0, 300,200);
     ctx.beginPath();
-    ctx.save();
 
     var temperatures = [];
-    var nextTemperatures = [];
-    var lastTemp = 20;
 
-    var heartRates = [];
-    var nextRates = [];
-    var lastRate = 30;
-
-    var gsrs = [];
-    var nextGsrs = [];
-    var lastGsr = 10;
-
-    for (var i = 0; i < 300; i++) {
-      gsrs.push(10);
-      temperatures.push(20);
-      heartRates.push(30);
+    for (var i = 0; i < 50; i++) {
+      temperatures[i]= i*2;
     }
 
+    // var heartRates = [];
+    // var nextRates = [];
+    // var lastRate = 30;
+    //
+    // var gsrs = [];
+    // var nextGsrs = [];
+    // var lastGsr = 10;
+
     setInterval(function(){
+      var copyTemps = temperatures.slice();
+
       ctx.clearRect(0, 0, 300, 200);
 
-      for (var i = 0; i < 300; i++) {
-        var tempValue = temperatures[i];
+      ctx.beginPath();
+      ctx.moveTo(0, 200 - copyTemps[0]);
+
+      for (var i = 1; i < 50; i++) {
+        var tempValue = copyTemps[i];
         // var rateValue = heartRates[i];
         // var gsrValue = gsrs[i];
 
-        ctx.fillStyle = '#f79420';
-        ctx.globalAlpha = 1.0 - (i/300.0);
-        ctx.fillRect(i+10, 200 - tempValue - 5, 2, 2);
+        ctx.strokeStyle = '#f79420';
+        ctx.globalAlpha = 1.0 - (i/50.0);
+        ctx.lineTo(i*6, 200 - tempValue);
+        ctx.stroke();
 
         // ctx.fillStyle = '#e55036';
         // ctx.fillRect(i+10, 200 - rateValue - 5, 2, 2);
@@ -56,64 +55,57 @@ $(document).ready(function () {
         // ctx.fillRect(i+10, 200 - gsrValue - 5, 2, 2);
 
       }
-    }, 50);
+      ctx.closePath();
+    }, 100);
   }
 
   setInterval(function(){
     if (testing) {
-      testtTemp = parseInt(Math.random()*100);
-      temperatures.push(testTemp);
+      testTemp = parseInt(Math.random()*20) + 80;
+      temperatures.unshift(testTemp);
+      if (temperatures.length > 100) {
+        temperatures.pop();
+      }
+      $("#temp-readout").html(testTemp);
     } else {
       $.ajax({
         url: 'api/temperatures',
         success: function(data){
           nextTemp = parseFloat(data.data);
 
-          processTransition(nextTemp, lastTemp, nextTemperatures, "#temp-readout");
-          lastTemp = nextTemp;
+          temperatures.unshift(nextTemp);
+
+          if (temperatures.length > 100) {
+            temperatures.pop();
+          }
+
+          $("#temp-readout").html(nextTemp);
         }
       });
     }
-  }, 1000);
+  }, 500);
 
-  setInterval(function(){
-    $.ajax({
-      url: 'api/heart_rates',
-      success: function(data){
-        var nextRate =  parseFloat(data.data);
-
-        processTransition(nextRate, lastRate, nextRates, ".heart-rate-container");
-        lastRate = nextRate;
-      }
-    });
-  }, 1000);
-
-  setInterval(function(){
-    $.ajax({
-      url: 'api/skin_responses',
-      success: function(data){
-        var nextGsr = parseFloat(data.data);
-
-        processTransition(nextGsr, lastGsr, nextGsrs, ".skin-response-container");
-        lastGsr = nextGsr;
-      }
-    });
-  }, 1000);
-
-  function processTransition(nextValue, lastValue, nextValueArray, readoutElement) {
-    if (nextValue != lastValue) {
-      $(readoutElement).html(nextValue).width(20);
-      var transValue = lastValue;
-
-      while (transValue != nextValue) {
-        if(transValue > nextValue) {
-          transValue = transValue - 0.5;
-        }
-        if(transValue < nextValue) {
-          transValue = transValue + 0.5;
-        }
-        nextValueArray.push(transValue);
-      }
-    }
-  }
+  // setInterval(function(){
+  //   $.ajax({
+  //     url: 'api/heart_rates',
+  //     success: function(data){
+  //       var nextRate =  parseFloat(data.data);
+  //
+  //       processTransition(nextRate, lastRate, nextRates, ".heart-rate-container");
+  //       lastRate = nextRate;
+  //     }
+  //   });
+  // }, 1000);
+  //
+  // setInterval(function(){
+  //   $.ajax({
+  //     url: 'api/skin_responses',
+  //     success: function(data){
+  //       var nextGsr = parseFloat(data.data);
+  //
+  //       processTransition(nextGsr, lastGsr, nextGsrs, ".skin-response-container");
+  //       lastGsr = nextGsr;
+  //     }
+  //   });
+  // }, 1000);
 });
